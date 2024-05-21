9c266c696848ff82aad55975aad53cf737e89caf
Ramy Kader <ramy.kader@Ramys-MacBook-Pro.local>
2024-05-21 19:47:00 -0400

    Initial commit

---
+++ my-api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const movies = [
+    { id: 1, title: "Inception", director: "Christopher Nolan" },
+    { id: 2, title: "The Matrix", director: "Lana Wachowski, Lilly Wachowski" },
+    { id: 3, title: "Interstellar", director: "Christopher Nolan" }
+];
+
+// API endpoint to get all movies
+app.get('/api/movies', (req, res) => {
+    res.json(movies);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
