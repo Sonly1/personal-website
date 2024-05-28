792804e05d2c4799f25980fc874161b6a3e5d92e
Ramy Kader <ramy.kader@Ramys-MacBook-Pro.local>
2024-05-28 00:00:12 -0400

    API Test

--- api.js
+++ api.js
@@ -4,8 +4,9 @@ const port = process.env.PORT || 3000;
 
 const movies = [
     { id: 1, title: "Inception", director: "Christopher Nolan" },
-    { id: 2, title: "The Matrix", director: "Lana Wachowski, Lilly Wachowski" },
-    { id: 3, title: "Interstellar", director: "Christopher Nolan" }
+    { id: 2, title: "Dune Part II", director: "Denis Villeneuve" },
+    { id: 3, title: "Tenet", director: "Christopher Nolan" }, 
+    {id: 4, title: "Lord Of The Ring", director: "Peter Jackson"}
 ];
 
 // API endpoint to get all movies
